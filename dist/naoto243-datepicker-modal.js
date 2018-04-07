2fbb59902a7e27d67a4c5792c349b2de726805e2
伏見尚人 <fushiminaoto@fushiminaotonoMacBook-Pro.local>
2018-04-08 02:29:27 +0900

    1st commit

---
+++ dist/naoto243-datepicker-modal.js
@@ -0,0 +1,6 @@
+import * as React from 'react';
+export default class Naoto243DatepickerModal extends React.Component {
+    render() {
+        return (React.createElement("div", null));
+    }
+}
---
+++ src/ts/naoto243-datepicker-modal.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+interface Props {
+  
+}
+
+interface State {
+  
+}
+
+export default class Naoto243DatepickerModal extends React.Component<Props, State>{
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
