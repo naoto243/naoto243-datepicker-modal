c1bce0c8460da7a011503ccf4a1b4191994ee463
伏見尚人 <fushiminaoto@fushiminaotonoMacBook-Pro.local>
2018-04-08 03:51:34 +0900

    fix compile

--- dist/modal.js
+++ dist/modal.js
@@ -1,33 +1,20 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var ReactDOM = require("react-dom");
-var Modal = /** @class */ (function (_super) {
-    __extends(Modal, _super);
-    function Modal(props) {
-        var _this = _super.call(this, props) || this;
-        _this.el = document.createElement("div");
-        return _this;
+const React = require("react");
+const ReactDOM = require("react-dom");
+class Modal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.el = document.createElement("div");
     }
-    Modal.prototype.componentDidMount = function () {
+    componentDidMount() {
         this.props.portalDOM.appendChild(this.el);
-    };
-    Modal.prototype.componentWillUnmount = function () {
+    }
+    componentWillUnmount() {
         this.props.portalDOM.removeChild(this.el);
-    };
-    Modal.prototype.render = function () {
+    }
+    render() {
         return ReactDOM.createPortal(this.props.children, this.el);
-    };
-    return Modal;
-}(React.Component));
+    }
+}
 exports.Modal = Modal;
